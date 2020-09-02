263b7ebb016aff72f2544037b09bbe5a0e77dcfc
jovan6jovan <jovan6jovanovic@gmail.com>
2020-09-02 14:41:29 +0200

    better folder structure and some styling

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Link, graphql, useStaticQuery } from "gatsby"
 
 // components
-import Layout from "../components/layout/layout";
+import Layout from "../../components/layout/layout";
+
+// styles
+import blogStyles from "./blog.module.scss";
 
 const Blog = () => {
   const data = useStaticQuery(graphql`
@@ -23,17 +26,15 @@ const Blog = () => {
     }
   `);
 
-  // console.log(data.allMarkdownRemark.edges);
-
   return (
     <Layout>
       <section>
         <h1>Blog</h1>
         <p>Ovde idu postovi</p>
-        <ol>
+        <ul className={blogStyles.posts}>
           {data.allMarkdownRemark.edges.map((post, idx) => {
             return (
-              <li key={idx}>
+              <li key={idx} className={blogStyles.post}>
                 <Link to={`/blog/${post.node.fields.slug}`}>
                   <h2>{post.node.frontmatter.title}</h2>
                   <p>{post.node.frontmatter.date}</p>
@@ -41,7 +42,7 @@ const Blog = () => {
               </li>
             )
           })}
-        </ol>
+        </ul>
       </section>
     </Layout>
   )
