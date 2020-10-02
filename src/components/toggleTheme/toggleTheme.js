9472bbdbf5135ab2de27d62575bbc1682d90b8a7
jovan6jovan <jovan6jovanovic@gmail.com>
2020-10-02 19:49:57 +0200

    small theme check

--- src/components/toggleTheme/toggleTheme.js
+++ src/components/toggleTheme/toggleTheme.js
@@ -7,22 +7,27 @@ import themeTogglerStyles from "./toggleTheme.module.scss"
 const ToggleTheme = () => {
   return (
     <ThemeToggler>
-      {({ theme, toggleTheme }) => (
-        <div className={themeTogglerStyles.togglerContainer}>
-            <input
-              type="checkbox"
-              onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-              checked={theme === "dark"}
-              className={themeTogglerStyles.checkbox}
-              id="check"
-            />{" "}
-          <label htmlFor="check" className={themeTogglerStyles.label}>
-            <span className={themeTogglerStyles.moon} role="img">🌙</span>
-		        <span className={themeTogglerStyles.sun} role="img">☀️</span>
-		        <div className={themeTogglerStyles.ball}></div>
-          </label>
-        </div>
-      )}
+      {({ theme, toggleTheme }) => {
+        if (theme == null) {
+          return null
+        }
+        
+        return (
+          <div className={themeTogglerStyles.togglerContainer}>
+              <input
+                type="checkbox"
+                onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
+                checked={theme === "dark"}
+                className={themeTogglerStyles.checkbox}
+                id="check"
+              />{" "}
+            <label htmlFor="check" className={themeTogglerStyles.label}>
+              <span className={themeTogglerStyles.moon} role="img">🌙</span>
+              <span className={themeTogglerStyles.sun} role="img">☀️</span>
+              <div className={themeTogglerStyles.ball}></div>
+            </label>
+          </div>
+        )}}
     </ThemeToggler>
   )
 }
